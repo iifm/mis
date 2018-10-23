ab40628ecfe1d7ca1ca5838e1eea93b9b2332bee
manish.ram@iifm.co.in <manish.ram@iifm.co.in>
2018-10-23 17:15:40 +0530

    oct 23rd

--- resources/views/mis/attendance/view.blade.php
+++ resources/views/mis/attendance/view.blade.php
@@ -141,14 +141,14 @@
                             ?>
 
                         </td>
-                        @if(($data['inTime'])=='NA' && ($data['outTime'])=='NA')
-                        @foreach($remarks as $remark)
-                        @if($data['date']==$remark['date'])    
-                        <td>{{$remark['remark']}}</td>
-                        @endif
-                        @endforeach
+                        @if($data['inTime']=='NA' && $data['outTime']=='NA')
+                           @foreach($remarks as $remark)
+                              @if($data['date']==$remark['date'])    
+                                 <td>{{$remark['remark']}}</td>
+                             @endif
+                           @endforeach
                         @else
-                        <td>Present</td>
+                          <td>Present</td>
                         @endif
 
                      <!--    <td><a href="{{url('update-attendance')}}/{{$data['user_id']}}/{{$data['date']}}" class="btn btn-warning fa fa-edit"></a></td>
